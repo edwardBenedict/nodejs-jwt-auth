5c888368147fd5fb05ad2945299144ee7ad185f2
ed ben <edwardbenedict92@gmail.com>
2021-04-20 20:51:37 +0300

    feat: add userSchema and auth

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
+// Import Routes
+const authRoute = require("./routes/auth");
+
+const app = express();
+
+// Connect to DB
+DB_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@trainingcluster.tpyei.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+mongoose
+  .connect(`${DB_URL}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to DB!"))
+  .catch((error) => console.log("Error : ", error));
+
+// Middlewares
+app.use(express.json());
+
+// Route Middlewares
+app.use("/api/user", authRoute);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
